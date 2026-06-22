4b549b94cc0a4ae12fdf1b00955b3233fb83fa00
Gabriel-Protocol <chetar.animation@gmail.com>
2026-06-22 21:13:59 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Gabriel-Protocol-Homepage/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
